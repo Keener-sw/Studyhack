3dfab397e0829cd6e36007738cf66d410f79b3fb
Keener-sw <cptkeener@icloud.com>
2025-04-14 03:05:12 +0900

    adjust folder

---
+++ www/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Future Burger</title>
+        <link rel="stylesheet" href="/css/style.css">
+    </head>
+    <body>
+        
+        <div class="middle">
+            <div class="title">Welcome to the Future Burger</div>
+                <img src="/img/mainburger.png" alt="로고" class="main-logo">
+        </div>
+        
+        <div class="top-buttons">
+            <a href="login.php" class="top-link">Login</a>
+            <a href="register.php" class="top-link">Register</a>
+        </div>
+
+
+    </body>
+</html>
+
+<?php 
+    
+?>

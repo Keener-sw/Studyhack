7e9d1da901307c44cea408e4f8d8691c9b8a9945
Keener-sw <cptkeener@icloud.com>
2025-07-03 02:07:27 +0900

    25.7.3

--- www/priv_page.php
+++ www/priv_page.php
@@ -11,7 +11,9 @@
         header('Location: index1.php');
         exit();
     }
+    $err = '';
 ?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -24,13 +26,35 @@
             <div class="middle">
                 <p id="id">ID :    <?php echo $user['id']; ?></p>
                 <p id="name">Name:    <?php echo $user['name']; ?></p>
-                <p style="display=flex; gap:10px">Nickname: <?php echo $user['nickname']; ?>
+                <p style="display=flex; gap:20px;">Nickname: <?php echo $user['nickname']; ?>
                 <input type="text" id="nickname" placeholder="Type new nickname!">
-                <button type="button" onclick="change_nick()">Change!</button>
+                <button type="button" onclick=change_nick()>Change!</button>
+                <p><?php echo $err ?></p>
             </p>
                 <p id="email">E-Mail: <?php echo $user['email']; ?></p>
             </div>
         </form>
+
+        <script>    
+            function change_nick(){
+                if(document.getElementById('nickname').value == '' || document.getElementById('nickname').value == $user['nickname']){
+                    $err = '새로운 닉네임을 입력해주세요.';
+                    return;
+                }
+                else{
+                    $err = 'something wrong';
+                }
+                fetch('change_nick.php', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    },
+                    body: new URLSearchParams({
+                        'nickname': document.getElementById('nickname').value
+                    })
+                })
+            }    
+        </script>
     </body>
     </head>
 </html>
